a01226e4b83158a6ce6c0fb9390e041157dcc461
Joseph Guzman <josephfguzman@gmail.com>
2020-03-25 13:43:22 -0700

    data_combiner.py greatly optimized by using a dictionary instead of DataFrame

--- data_combiner.py
+++ data_combiner.py
@@ -13,11 +13,12 @@ import plotly.express as px
 from urllib.request import urlopen
 import argparse
 import glob, os
+import pickle
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(
-        description='Combine two .pkl files'
+        description='Combine a direcotry of .pkl files'
     )
     parser.add_argument("input_dir",
                         help="directory to search for .pkl files to combine")
@@ -25,37 +26,36 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     pattern = os.path.join(args.input_dir, "*.pkl")
-    
-    all_pd = pd.DataFrame()
+
+    all_pd = pd.DataFrame(columns=['fips', 'name', 'hits'])
+    # indexing a dict is about 100x faster than a DataFrame
+    all_hits = dict()
     count = 0
     print("about to enter pattern")
     my_glob = glob.glob(pattern)
     for f in my_glob:
-        if(count == 0):
-            all_pd = pd.read_pickle(f)
-        else:
-            pd1 = pd.read_pickle(f)
-            unique_fips = pd.concat([all_pd['fips'], pd1['fips']]).unique()
-            new_pd = pd.DataFrame(columns=['fips', 'name', 'hits'])
-            new_pd['fips'] = new_pd['fips'].astype(int)
-            new_pd['hits'] = new_pd['hits'].astype(int)
-            for fips in unique_fips:
-                search = all_pd[all_pd['fips'] == fips]
-                success = False
-                if(search.shape[0] == 1):
-                    success = True
-                    index = search.index[0]
-                    new_pd = new_pd.append(all_pd.iloc[index], ignore_index=True)
-                search = pd1[pd1['fips'] == fips]
-                if(search.shape[0] == 1):
-                    index = search.index[0]
-                    if(success):
-                        new_pd['hits'][new_pd.shape[0] - 1] += pd1['hits'][index]
-                    else:
-                        new_pd = new_pd.append(pd1.iloc[index], ignore_index=True)
-            all_pd = new_pd
+        small_pd = pd.read_pickle(f)
+        for i in range(small_pd.shape[0]):
+            fips = small_pd['fips'].loc[i]
+            name = small_pd['name'].loc[i]
+            hits = small_pd['hits'].loc[i]
+            # try and except should be faster than checking if key exists
+            try:
+                all_hits[fips][0] += hits
+            except:
+                all_hits[fips] = [hits, name]
         count += 1
         print(f"processed {count} files out of {len(my_glob)}")
 
+    # copy dict info to all_pd
+    for key in all_hits:
+        fips = str(key)
+        fips = fips.zfill(5) # 0 padded
+        hits = all_hits[key][0]
+        name = all_hits[key][1] + " County"
+        all_pd = all_pd.append({'fips':fips, 'name':name, 'hits':hits},
+                      ignore_index=True)
+
+    all_pd = all_pd.astype({'fips':'str', 'name':'str', 'hits':'int'})
     print(f"writing to file")
     all_pd.to_pickle(args.out_file)
